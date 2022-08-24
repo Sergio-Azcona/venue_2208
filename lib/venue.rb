2f9f0ab743e8f6b7aca3c0532650b9a6bcb8a5f9
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2022-08-24 11:46:07 -0400

    feat: through add_patron method complete

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,16 @@
 class Venue
+  attr_reader :name, :capacity, :patrons
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = Array.new
+  end
+
+  def add_patron(person)
+    @patrons << person
+  end
+
+
+
+
 end
